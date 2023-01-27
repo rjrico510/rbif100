3a0539463a4e537a191ce16d5dd5e8604b20f4d7
Rudy Rico <rrico@brandeis.edu>
2023-01-27 18:05:32 +0000

    cleanup arithmetic (SC2288)

--- bash_tutorial/04_arithmetic/arithmetic_03_04.sh
+++ bash_tutorial/04_arithmetic/arithmetic_03_04.sh
@@ -3,6 +3,6 @@ LOWER=${1:-0}
 UPPER=${2:-100}
 # add 1 for mod; set baseline
 # e.g. for 10-45, get 0-35 and add 10
-UPPERMOD=$(( ${UPPER} - ${LOWER} + 1 ))
-RESULT=$(( ${RANDOM} % ${UPPERMOD} + ${LOWER} ))
+UPPERMOD=$((UPPER - LOWER + 1))
+RESULT=$((RANDOM % UPPERMOD + LOWER))
 echo "${RESULT}"
