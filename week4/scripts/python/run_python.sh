0a043b64394a1c3b8409c9ba4c196dca98cfa54e
Rudy Rico <rrico@brandeis.edu>
2023-02-05 03:33:05 +0000

    Add report generation to python version script

--- week4/scripts/python/run_python.sh
+++ week4/scripts/python/run_python.sh
@@ -3,3 +3,4 @@ python3 copyExomes.py ../../data/assignment/clinical_data.txt ../../data/assignm
 python3 createCrisprReady.py ../../analysis/assignment_python/exomesCohort ../../data/assignment/motif_list.txt ../../analysis/assignment_python/CrisprReady > ../../analysis/assignment_python/createCrisprReady.txt
 python3 identifyCrisprSite.py ../../analysis/assignment_python/CrisprReady ../../analysis/assignment_python/PreCrispr > ../../analysis/assignment_python/identifyCrisprSite.txt
 python3 editGenome.py ../../analysis/assignment_python/PreCrispr ../../analysis/assignment_python/PostCrispr > ../../analysis/assignment_python/editGenome.txt
+python3 ../exomeReport.py ../../data/assignment/clinical_data.txt ../../analysis/assignment_python/PostCrispr --report ../../analysis/assignment_python/exomeReport.txt
