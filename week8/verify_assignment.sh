40a8578d2ca5e02dd28ccffcb4e26680a086bdfe
Rudy Rico <rrico@brandeis.edu>
2023-03-03 04:38:38 +0000

    test both outputs

--- week8/verify_assignment.sh
+++ week8/verify_assignment.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -xe
 diff /home/ricor/week8/MC1R_gene_AA.fasta analysis/assignment/baseline/MC1R_gene_AA.fasta
+diff /home/ricor/week8/MC1R_homology_list.txt analysis/assignment/baseline/MC1R_homology_list.txt
