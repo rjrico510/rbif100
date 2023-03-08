2c622ff0e360d54c495b48090adca3d00fd7d7bd
Rudy Rico <rrico@brandeis.edu>
2023-03-08 21:53:43 +0000

    pipeline boilerplate

---
+++ week10/scripts/pipeline.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python3
+"""TODO - top-level docstring
+
+"""
+
+import numpy as np
+import pandas as pd
+
+def main():
+    """main
+    """
+
+if __name__ == "__main__":
+    main()
