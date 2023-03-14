bc40662e0b831f5ebc35f733e5892d2bf97011e7
Rudy Rico <rrico@brandeis.edu>
2023-03-14 01:43:29 +0000

    Set up assignment staging

---
+++ week10/verify_assignment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -xe
+diff /home/ricor/week10/buffalo.pdf analysis/assignment/baseline/buffalo.pdf
+diff /home/ricor/week10/fox.pdf analysis/assignment/baseline/fox.pdf
+diff /home/ricor/week10/lion.pdf analysis/assignment/baseline/lion.pdf
+diff /home/ricor/week10/clinical_data_with_diversity.txt analysis/assignment/baseline/clinical_data_with_diversity.txt
